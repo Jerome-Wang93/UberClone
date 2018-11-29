840a5d7c1dfe2a4f33096b46ad27d731af8ae52f
jeromewang <jerome.wang1024@gmail.com>
2018-11-29 22:16:19 +1300

    log error

--- app/src/main/java/com/example/jerome/uberclone/MainActivity.kt
+++ app/src/main/java/com/example/jerome/uberclone/MainActivity.kt
@@ -2,11 +2,43 @@ package com.example.jerome.uberclone
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Switch
+import com.parse.ParseAnonymousUtils
+import com.parse.ParseUser
 
 class MainActivity : AppCompatActivity() {
+
+    fun getStart(v : View){
+        var switch = findViewById<Switch>(R.id.switch1)
+        Log.i("Switch value", switch.isChecked.toString())
+        var usertype = "Rider"
+        if ( switch.isChecked){
+            usertype = "Driver"
+        }
+        ParseUser.getCurrentUser().put("Rider or Driver",usertype)
+        Log.i("Info","Hello " + usertype)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getSupportActionBar()?.hide()
+
+        if( ParseUser.getCurrentUser() == null){
+            ParseAnonymousUtils.logIn { user, e ->
+                if (e == null){
+                    Log.i("Info","Log in successful")
+                }else{
+                    Log.i("Info",e.message)
+                }
+            }
+        }else{
+            if (ParseUser.getCurrentUser().get("Rider or Driver") != null){
+                Log.i("Info",ParseUser.getCurrentUser().get("Rider or Driver").toString())
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/example/jerome/uberclone/StartServer.java
@@ -0,0 +1,17 @@
+package com.example.jerome.uberclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class StartServer extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("2f69089a7249521d74763fb010a578d8a3a20865")
+                .clientKey("af1572c13b73e766108cfc242826800d7fd9b126")
+                .server("http://13.211.115.155:80/parse/")
+                .build());
+    }
+}

2821676cc225bcafee818ab0a1652639a6c7f272
jeromewang <jerome.wang1024@gmail.com>
2018-12-06 23:33:52 +1300

    need fix the decode error

--- app/src/main/java/com/example/jerome/uberclone/RiderActivity.kt
+++ app/src/main/java/com/example/jerome/uberclone/RiderActivity.kt
@@ -12,6 +12,8 @@ import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.util.Log
+import android.view.View
+import android.widget.Button
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,12 +21,16 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.parse.ParseGeoPoint
+import com.parse.ParseObject
+import com.parse.ParseQuery
+import com.parse.ParseUser
 import java.util.jar.Manifest
 
-class RiderActivity : AppCompatActivity(), OnMapReadyCallback {
+abstract class RiderActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
-
+    var requestActive = false
 
     fun update(location : Location){
         var userLocation =  LatLng(location.latitude,location.longitude)
@@ -33,7 +39,62 @@ class RiderActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap.addMarker(MarkerOptions().position(userLocation).title("Your location"))
     }
 
+    var locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    var locationListener = object : LocationListener{
+        override fun onLocationChanged(location: Location) {
+            update(location)
+        }
 
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+
+        }
+    }
+
+    fun callUber(v : View){
+        var button = findViewById(R.id.call) as Button
+        if (requestActive){
+            var query = ParseQuery<ParseObject>("Request")
+            query.whereEqualTo("username",ParseUser.getCurrentUser().username)
+            query.findInBackground { objects, e ->
+                if (e == null){
+                    if ( objects.size > 0){
+                        for (obj in objects){
+                            obj.delete()
+                        }
+                        button.setText("Call An Uber")
+                    }
+                }
+            }
+        }else{
+            if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1)
+            }else{
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0F,locationListener)
+                var lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                if (lastLocation != null){
+                    var request = ParseObject("Request")
+                    request.put("username",ParseUser.getCurrentUser().username)
+                    var location = ParseGeoPoint(lastLocation.latitude,lastLocation.longitude)
+                    request.put("Location",location)
+                    request.saveInBackground {
+                        if (it == null){
+                            button.setText("Cancel Uber")
+                        }else{
+                            Log.i("Info","Request save error: " + it.message)
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
@@ -41,24 +102,6 @@ class RiderActivity : AppCompatActivity(), OnMapReadyCallback {
         if (requestCode == 1){
             if (grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
                 if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
-                    var locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-                    var locationListener = object : LocationListener{
-                        override fun onLocationChanged(location: Location) {
-                            update(location)
-                        }
-
-                        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-
-                        }
-
-                        override fun onProviderDisabled(provider: String?) {
-
-                        }
-
-                        override fun onProviderEnabled(provider: String?) {
-
-                        }
-                    }
                     locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0F,locationListener)
                     var lastLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
                     update(lastLocation)
@@ -89,30 +132,10 @@ class RiderActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        var locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        var locationListener = object : LocationListener{
-            override fun onLocationChanged(location: Location) {
-                update(location)
-            }
-
-            override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
-
-            }
-
-            override fun onProviderDisabled(provider: String?) {
-
-            }
-
-            override fun onProviderEnabled(provider: String?) {
-
-            }
-        }
-
         if (Build.VERSION.SDK_INT < 23){
             locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0F,locationListener)
         }else{
-            var permission = android.Manifest.permission.ACCESS_FINE_LOCATION
-            if (ContextCompat.checkSelfPermission(this,permission) != PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this,android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
                 ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),1)
             }else{
                 locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0F,locationListener)

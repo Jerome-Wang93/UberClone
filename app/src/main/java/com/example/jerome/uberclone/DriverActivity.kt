e4ff24efcd4e6c4862acdcc8afd9a102b0842424
jeromewang <jerome.wang1024@gmail.com>
2018-12-12 22:59:48 +1300

    add location manager

--- app/src/main/java/com/example/jerome/uberclone/DriverActivity.kt
+++ app/src/main/java/com/example/jerome/uberclone/DriverActivity.kt
@@ -1,5 +1,9 @@
 package com.example.jerome.uberclone
 
+import android.content.Context
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
@@ -9,9 +13,30 @@ class DriverActivity : AppCompatActivity() {
 
     var requests  =  arrayListOf<String>()
 
+    var locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+    var locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+        }
+
+        override fun onProviderDisabled(provider: String?) {
+
+        }
+
+        override fun onProviderEnabled(provider: String?) {
+
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_driver)
+        setTitle("Nearby Request")
 
         var requestList = findViewById<ListView>(R.id.request_list)
         requests.add("test")
